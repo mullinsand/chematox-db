c22027d630329c43ec2603d1e022954e409eb1cf
Andrew Mullins <mullinsand15@gmail.com>
2023-03-09 12:02:29 -0700

    removed unnecessary comments

--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
-# Test suite for the Todo model
 RSpec.describe Sample, type: :model do
   # Association test
-  # ensure Todo model has a 1:m relationship with the Item model
   it { should have_many(:results).dependent(:destroy) }
   # Validation tests
-  # ensure columns title and created_by are present before saving
 end
